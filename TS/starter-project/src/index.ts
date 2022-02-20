40d3601cc525a09eda573f24dae4ec395cb97029
Kevin KOUOMEU <1kevinson@Kevins-MacBook-Pro.local>
2022-02-20 22:00:06 +0100

    Typescript starter project

---
+++ TS/starter-project/src/index.ts
@@ -0,0 +1,15 @@
+const java: string = 'JAVA';
+const javascript: string = 'JAVASCRIPT';
+
+function getFavouriteLanguage(answer: string): string {
+  switch (answer.toUpperCase()) {
+    case java.toString():
+      return `Your favourite language is ${java}`;
+    case javascript.toString():
+      return `Your favourite language is ${javascript}`;
+    default:
+      return 'wrong language...';
+  }
+}
+
+module.exports = getFavouriteLanguage;
