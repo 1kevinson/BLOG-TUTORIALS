c069890b09021820d90a943321653f859e6f1e2c
1kevinson <kevinkouomeu@icloud.com>
2022-02-21 08:16:19 +0100

    Update producer convert jsonMessage before send POST Request

--- Java/producer-app/src/main/java/com/demo/broker/producer/Producer.java
+++ Java/producer-app/src/main/java/com/demo/broker/producer/Producer.java
@@ -1,6 +1,8 @@
 package com.demo.broker.producer;
 
 import com.demo.broker.model.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
@@ -9,15 +11,17 @@ import org.springframework.stereotype.Service;
 public class Producer {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper mapper;
 
     @Value("${spring.artemis.embedded.queues}")
     private String queueDestination;
 
-    public Producer(JmsTemplate jmsTemplate) {
+    public Producer(JmsTemplate jmsTemplate, ObjectMapper mapper) {
         this.jmsTemplate = jmsTemplate;
+        this.mapper = mapper;
     }
 
-    public void send(Message message) {
-        jmsTemplate.convertAndSend(queueDestination, message);
+    public void send(Message message) throws JsonProcessingException {
+        jmsTemplate.convertAndSend(queueDestination, mapper.writeValueAsString(message));
     }
 }
