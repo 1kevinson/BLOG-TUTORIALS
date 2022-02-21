2b4b25a9b0ce5f15be952e061c43612fee5747aa
1kevinson <kevinkouomeu@icloud.com>
2022-02-21 08:51:44 +0100

    Implement consumer app + remove object mapper

--- Java/consumer-app/src/main/java/com/demo/broker/consumer/MessageConsumer.java
+++ Java/consumer-app/src/main/java/com/demo/broker/consumer/MessageConsumer.java
@@ -1,7 +1,5 @@
 package com.demo.broker.consumer;
 
-import com.demo.broker.model.Message;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
@@ -11,11 +9,6 @@ import org.springframework.stereotype.Component;
 public class MessageConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
-    private final ObjectMapper mapper;
-
-    public MessageConsumer(ObjectMapper mapper) {
-        this.mapper = mapper;
-    }
 
     @JmsListener(destination = "${spring.artemis.embedded.queues}")
     public void messageListener(String message){
