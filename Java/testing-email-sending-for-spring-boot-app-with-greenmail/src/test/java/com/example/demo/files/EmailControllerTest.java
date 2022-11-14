d8db6740f1ff9ac83ad12634b9cbca6a268f8de0
Kevin Kouomeu <kevinkouomeu@icloud.com>
2022-11-14 01:10:08 +0100

    Integration Test for email sending with GreenMail and Junit 5

--- Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/test/java/com/example/demo/files/EmailControllerTest.java
+++ Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/test/java/com/example/demo/files/EmailControllerTest.java
@@ -4,6 +4,8 @@ import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -34,14 +36,21 @@ class EmailControllerTest {
     private TestRestTemplate testRestTemplate;
 
     @Test
-    void should_send_email_to_user() throws MessagingException {
-        String emailPayload = "{\"email\": \"tester@spring.com\", \"content\": \"Hello this is an email\"}";
+    void should_send_email_to_user() throws MessagingException, JSONException {
+        // Arrange
+        JSONObject emailJsonObject = new JSONObject();
+        emailJsonObject.put("email","tester@spring.com");
+        emailJsonObject.put("content","Hello this is a simple email message");
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> emailRequest = new HttpEntity<>(emailPayload, headers);
+        HttpEntity<String> emailRequest = new HttpEntity<>(emailJsonObject.toString(), headers);
+
+        // Act
         ResponseEntity<Void> response = testRestTemplate.postForEntity("/notify/user", emailRequest, Void.class);
 
+
+        // Assert
         assertEquals(200, response.getStatusCodeValue());
 
         MimeMessage receivedMessage = greenMail.getReceivedMessages()[0];
@@ -49,7 +58,7 @@ class EmailControllerTest {
         assertEquals("tester@spring.com", receivedMessage.getAllRecipients()[0].toString());
         assertEquals("test.sender@hotmail.com", receivedMessage.getFrom()[0].toString());
         assertEquals("Message from Java Mail Sender", receivedMessage.getSubject());
-        assertEquals("Hello this is an email", GreenMailUtil.getBody(receivedMessage));
+        assertEquals("Hello this is a simple email message", GreenMailUtil.getBody(receivedMessage));
     }
 }
 
