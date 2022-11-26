a06e54ec8820b45c220b9eb676c7436dcf04be2e
Kevin Kouomeu <kevinkouomeu@icloud.com>
2022-11-26 14:38:41 +0100

    :recycle: Change validation imports

--- Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/main/java/com/example/demo/files/EmailController.java
+++ Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/main/java/com/example/demo/files/EmailController.java
@@ -1,12 +1,11 @@
 package com.example.demo.files;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
