ff4ae7819d671f4083fab1a228b66b55ca4c7e93
Kevin Kouomeu <kevinkouomeu@icloud.com>
2022-11-26 13:36:08 +0100

    :zap: Update Validation dependency

--- Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/main/java/com/example/demo/files/EmailRequest.java
+++ Java/testing-email-sending-for-spring-boot-app-with-greenmail/src/main/java/com/example/demo/files/EmailRequest.java
@@ -1,10 +1,9 @@
 package com.example.demo.files;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
