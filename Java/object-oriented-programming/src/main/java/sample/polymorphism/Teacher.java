58fb636f03cb6499b9a31a60faf323aa1b5c4a02
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-01-09 01:43:48 +0100

    Polymorphism

--- Java/object-oriented-programming/src/main/java/sample/polymorphism/Teacher.java
+++ Java/object-oriented-programming/src/main/java/sample/polymorphism/Teacher.java
@@ -3,7 +3,6 @@ package sample.polymorphism;
 import java.util.List;
 
 public interface Teacher {
-
     public void speak();
     public void planningLesson();
     public void answerQuestion(List<Student> students);
