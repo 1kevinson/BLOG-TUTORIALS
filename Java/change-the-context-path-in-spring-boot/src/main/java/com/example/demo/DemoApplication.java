7f2a0c890c3a2ec2a6cb80ae9ce4bc3d96514311
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-03-25 15:32:59 +0100

    staged changs in vscode IDE

--- Java/change-the-context-path-in-spring-boot/src/main/java/com/example/demo/DemoApplication.java
+++ Java/change-the-context-path-in-spring-boot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path", "/tuto-demo");
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
