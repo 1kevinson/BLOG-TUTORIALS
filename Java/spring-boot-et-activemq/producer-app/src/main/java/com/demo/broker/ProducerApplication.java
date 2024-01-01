f4158c8e6e56f56bf4398afea34c87631ebba8d2
1kevinson <kevinkouomeu@icloud.com>
2024-01-01 08:29:22 +0100

    Refactored the apps names

--- Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/ProducerApplication.java
+++ Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/ProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrokerApplication {
+public class ProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrokerApplication.class, args);
+		SpringApplication.run(ProducerApplication.class, args);
 	}
 
 }
