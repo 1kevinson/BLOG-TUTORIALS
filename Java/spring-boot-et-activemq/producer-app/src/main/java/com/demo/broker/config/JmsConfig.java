db54177cba24161f28ff8c548a32762088abc27f
1kevinson <kevinkouomeu@icloud.com>
2024-01-01 16:51:07 +0100

    :ambulance: fix du producer et consumer

--- Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/config/JmsConfig.java
+++ Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/config/JmsConfig.java
@@ -13,11 +13,9 @@ public class JmsConfig {
 
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
-        DefaultJmsListenerContainerFactory jmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
-
+        var jmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
         jmsListenerContainerFactory.setConnectionFactory(connectionFactory);
         jmsListenerContainerFactory.setConcurrency("2-4");
-
         return jmsListenerContainerFactory;
     }
 
--- Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/controller/PublisherController.java
+++ Java/spring-boot-et-activemq/producer-app/src/main/java/com/demo/broker/controller/PublisherController.java
@@ -2,6 +2,7 @@ package com.demo.broker.controller;
 
 import com.demo.broker.model.Message;
 import com.demo.broker.producer.Producer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,20 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/message")
 public class PublisherController {
 
     private final Producer producer;
 
-    public PublisherController(Producer producer) {
-        this.producer = producer;
-    }
-
-    @PostMapping("/publish")
     public ResponseEntity<String> publish(@RequestBody Message message) {
         try {
             producer.send(message);
-            return new ResponseEntity<>("Sent", HttpStatus.OK);
+            return new ResponseEntity<>("Message Sent", HttpStatus.OK);
         } catch (Exception exception) {
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
