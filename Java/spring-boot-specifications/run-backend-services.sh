3d081b40d0eec8aadb4bd1f135d7681d7837e045
Kevin Kouomeu <kevinkouomeu@icloud.com>
2024-02-15 00:19:21 +0100

    delete unused project

--- Java/spring-boot-specifications/run-backend-services.sh
+++
@@ -1,9 +0,0 @@
-docker stop jpa-specifications postgres-demo &&
-
-docker rm jpa-specifications postgres-demo &&
-
-./mvnw clean package -DskipTests &&
-
-docker compose build --no-cache &&
-
-docker compose up -d
