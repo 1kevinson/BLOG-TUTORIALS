8bf3f2e98604e0489f1dfeace355161d77f86f3e
Kevin Kouomeu <kevinkouomeu@icloud.com>
2024-03-31 21:19:25 +0200

    delete logger in main class

--- Java/spring-boot-and-specifications/src/main/java/com/example/demo/DemoApplication.java
+++ Java/spring-boot-and-specifications/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,8 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
