ea42d9e25a293176668a4cd2b56030524a267fcd
1kevinson <kevinkouomeu@icloud.com>
2024-06-22 00:05:07 +0200

    📁 Add data.json and implement data fetching in App.js
    A new JSON file (data.json) housing the application's data has been added to the public folder. In src/App.js, the useEffect and useState hooks were used to retrieve and store this data respectively.

--- Javascript/read-the-content-of-a-json-file-in-react-component/src/App.js
+++ Javascript/read-the-content-of-a-json-file-in-react-component/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.PUBLIC_URL + '/data.json')
+        .then(response => response.json())
+        .then(jsonData => setData(jsonData))
+        .catch((error) => console.error('Error reading JSON file →', error));
+  }, []);
+
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*  Demo code */}
     </div>
   );
 }
