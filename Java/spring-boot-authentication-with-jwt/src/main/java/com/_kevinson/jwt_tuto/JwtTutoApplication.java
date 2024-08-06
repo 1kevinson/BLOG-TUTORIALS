e8ac59c727615452419d29e53761808977fa08cf
1kevinson <kevinkouomeu@icloud.com>
2024-08-06 08:15:10 +0200

    Initialize Spring Boot project with Maven Wrapper
    Added basic Spring Boot application structure with `JwtTutoApplication`, Maven wrapper files for consistent build environment, and initial dependencies in `pom.xml`. Included a `.gitignore` to exclude unnecessary files from version control.

---
+++ Java/spring-boot-authentication-with-jwt/src/main/java/com/_kevinson/jwt_tuto/JwtTutoApplication.java
@@ -0,0 +1,13 @@
+package com._kevinson.jwt_tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutoApplication.class, args);
+	}
+
+}
---
+++ Java/spring-boot-authentication-with-jwt/src/test/java/com/_kevinson/jwt_tuto/JwtTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._kevinson.jwt_tuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
